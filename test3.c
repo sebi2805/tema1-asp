a5f3e4427fa1c06886d969195048c40e1948642b
Sebi <sebastian.virtopeanu@cst.ro>
2024-04-12 16:10:45 +0300

    added changes

--- test3.c
+++ test3.c
@@ -4,6 +4,39 @@
 
 #define N 200
 
+void compute_nominator(double *sub_mat, double *sub_x_vec, double *sub_y_vec, int elements_per_proc, double *local_sum_product) {
+    *local_sum_product = 0.0;
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i < elements_per_proc; i++) {
+            *local_sum_product += sub_x_vec[i] * sub_mat[i * N + j] * sub_y_vec[j];
+        }
+    }
+}
+
+void compute_denominator(double *sub_x_vec, double *sub_y_vec, int rows_per_proc, double *local_sum_product_2) {
+    *local_sum_product_2 = 0.0;
+    for (int i = 0; i < rows_per_proc; i++) {
+        *local_sum_product_2 += sub_x_vec[i] * sub_y_vec[i];
+    }
+}
+
+void compute_all(double *local_nominator, double *local_denominator, double *global_nominator, double *global_denominator, MPI_Comm comm) {
+    MPI_Reduce(local_nominator, global_nominator, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
+    MPI_Reduce(local_denominator, global_denominator, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    if (rank == 0) {
+        printf("Total sum of matrix-vector products: %f\n", *global_nominator);
+        printf("Total sum of vector elements: %f\n", *global_denominator);
+        if (*global_denominator != 0) { // Check for divide-by-zero
+            printf("Fraction result: %f\n", *global_nominator / *global_denominator);
+        } else {
+            printf("Cannot compute fraction due to division by zero.\n");
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
@@ -52,38 +85,19 @@ int main(int argc, char *argv[]) {
 
     // Distribuirea matricei
     MPI_Scatter(mat_data, rows_per_proc * N, MPI_DOUBLE, sub_mat, rows_per_proc * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // Distribuirea vectorului
     MPI_Scatter(vec_x_data, elements_per_proc, MPI_DOUBLE, sub_x_vec, elements_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Scatter(vec_y_data, N, MPI_DOUBLE, sub_y_vec, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 
-    // De aici, fiecare proces poate opera pe sub_mat și sub_vec
-    double local_sum_product = 0.0, total_sum_product = 0.0;
-    double local_sum_product_2 = 0.0, total_sum_product_2 = 0.0;
+
     // Calculează suma pentru submatrice
-    for (int j = 0; j < N; j++) {
-        for (int i = 0; i < elements_per_proc; i++) {   
-            local_sum_product += sub_x_vec[i] * sub_mat[i * N + j] * sub_y_vec[j];
-        }
-    } 
+    double local_nominator = 0.0, global_nominator = 0.0;
+    double local_denominator = 0.0, global_denominator = 0.0;
 
-    for (int i = 0; i < rows_per_proc; i++) {
-        local_sum_product_2 += sub_x_vec[i] * sub_y_vec[i];
-    }
-
-    // Reducerea sumelor vectorilor la procesul cu rank-ul 0
-    MPI_Reduce(&local_sum_product_2, &total_sum_product_2, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    // Reducerea sumelor matricelor la procesul cu rank-ul 0
-    MPI_Reduce(&local_sum_product, &total_sum_product, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    // Procesul cu rank-ul 0 afișează sumele totale
-    if (rank == 0) {
-        printf("Suma totală a elementelor vectorului este: %f\n", total_sum_product);
-        printf("Suma totală a elementelor vectorului este: %f\n", total_sum_product_2);
-        printf("Suma totală a elementelor vectorului este: %f\n", total_sum_product/total_sum_product_2);
-
-    }
+    compute_nominator(sub_mat, sub_x_vec, sub_y_vec, elements_per_proc, &local_nominator);
+    compute_denominator(sub_x_vec, sub_y_vec, rows_per_proc, &local_denominator);
+    compute_all(&local_nominator, &local_denominator, &global_nominator, &global_denominator, MPI_COMM_WORLD);
+   
     // Eliberare memorie și finalizare
     if (rank == 0) {
         free(mat_data);
